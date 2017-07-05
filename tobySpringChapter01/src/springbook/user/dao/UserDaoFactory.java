d4ea28da56c037a0306fafd9e0b05c1a00360cab
DB400S3A <DB400S3A@10.231.4.40>
2017-07-05 10:57:26 +0900

    Add Spring Configuration

--- tobySpringChapter01/src/springbook/user/dao/UserDaoFactory.java
+++ tobySpringChapter01/src/springbook/user/dao/UserDaoFactory.java
@@ -1,10 +1,20 @@
 package springbook.user.dao;
 
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
 public class UserDaoFactory {
+	@Bean
 	public UserDao userDao(){
-		ConnectionMaker connectionMaker = new SimpleConnectionMaker();
-		UserDao userDao = new UserDao(connectionMaker);
+		UserDao userDao = new UserDao(connectionMaker());
 		return userDao;
+	}
+
+	@Bean
+	private ConnectionMaker connectionMaker() {
+		ConnectionMaker connectionMaker = new SimpleConnectionMaker();
+		return connectionMaker;
 	}
 
 }
--- tobySpringChapter01/src/springbook/user/dao/UserDaoTest.java
+++ tobySpringChapter01/src/springbook/user/dao/UserDaoTest.java
@@ -2,13 +2,18 @@ package springbook.user.dao;
 
 import java.sql.SQLException;
 
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
 import springbook.user.domain.User;
 
 public class UserDaoTest {
 	
 	public static void main(String[] args) throws ClassNotFoundException, SQLException{
 		
-		UserDao dao = new UserDaoFactory().userDao();
+		ApplicationContext context = new AnnotationConfigApplicationContext(UserDaoFactory.class);
+		
+		UserDao dao = context.getBean("userDao", UserDao.class);
 		
 		User user = new User();
 		user.setId("whiteship");
