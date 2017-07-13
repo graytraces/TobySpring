b5c1ea1483b6f8f58507f71c823760d1aabb388d
DB400S3A <DB400S3A@10.231.4.40>
2017-07-13 17:19:44 +0900

    JUnit test added

--- tobySpringChapter01/src/springbook/user/dao/UserDaoTest.java
+++ tobySpringChapter01/src/springbook/user/dao/UserDaoTest.java
@@ -1,7 +1,12 @@
 package springbook.user.dao;
 
+import static org.hamcrest.CoreMatchers.is;
+import static org.junit.Assert.assertThat;
+
 import java.sql.SQLException;
 
+import org.junit.Test;
+import org.junit.runner.JUnitCore;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.GenericXmlApplicationContext;
 
@@ -9,6 +14,33 @@ import springbook.user.domain.User;
 
 public class UserDaoTest {
 	
+	public static void main(String[] args){
+		JUnitCore.main("springbook.user.dao.UserDaoTest");
+	}
+		
+	@Test
+	public void addAndGet() throws SQLException, ClassNotFoundException{
+		ApplicationContext context = new GenericXmlApplicationContext("applicationContext.xml");
+		
+		UserDao dao = context.getBean("userDao", UserDao.class);
+		
+		User user = new User();
+		user.setId("whiteship");
+		user.setName("백기선");
+		user.setPassword("married");
+		
+		dao.add(user);
+		
+		System.out.println(user.getId() + "등록성공");
+		
+		User user2 = dao.get(user.getId());
+		
+		assertThat(user2.getName(), is(user.getName()));
+		assertThat(user2.getPassword(), is(user.getPassword()));
+		
+	}
+	
+	/*
 	public static void main(String[] args) throws ClassNotFoundException, SQLException{
 		
 		ApplicationContext context = new GenericXmlApplicationContext("applicationContext.xml");
@@ -36,5 +68,6 @@ public class UserDaoTest {
 		}
 		
 	}
+	*/
 
 }
