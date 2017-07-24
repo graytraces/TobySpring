9aae8c98b13ccbeecb13ad6988b3c2566190be06
DB400S3A <DB400S3A@10.231.4.40>
2017-07-24 12:32:08 +0900

    Change Datasource for test. And add @DirtiesContext.

--- tobySpringChapter01/src/springbook/user/dao/UserDaoTest.java
+++ tobySpringChapter01/src/springbook/user/dao/UserDaoTest.java
@@ -5,6 +5,8 @@ import static org.junit.Assert.assertThat;
 
 import java.sql.SQLException;
 
+import javax.sql.DataSource;
+
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.JUnitCore;
@@ -12,6 +14,8 @@ import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.ApplicationContext;
 import org.springframework.dao.EmptyResultDataAccessException;
+import org.springframework.jdbc.datasource.SingleConnectionDataSource;
+import org.springframework.test.annotation.DirtiesContext;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
@@ -20,6 +24,7 @@ import springbook.user.domain.User;
 
 @RunWith(SpringJUnit4ClassRunner.class)
 @ContextConfiguration(locations="/applicationContext.xml")
+@DirtiesContext
 public class UserDaoTest {
 	private UserDao dao;
 	private User user1;
@@ -41,6 +46,11 @@ public class UserDaoTest {
 	public void setUp() {
 		
 		this.dao = context.getBean("userDao", UserDao.class);
+		
+		DataSource dataSource = new SingleConnectionDataSource(
+				"jdbc:mysql://cardi.iptime.org:6306/springtest", "springtest", "springtest1234", true);
+		
+		dao.setDataSource(dataSource);
 		
 		this.user1 = new User("gyumee", "박성철", "springno1");
 		this.user2 = new User("leegw700", "이길원", "springno2");
