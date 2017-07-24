723cc204b00d26d42d0cc1b5a4b889e348d8bc69
DB400S3A <DB400S3A@10.231.4.40>
2017-07-24 12:22:08 +0900

    Use Spring-JUnit Context. Optimize context generation.

--- tobySpringChapter01/src/springbook/user/dao/UserDaoTest.java
+++ tobySpringChapter01/src/springbook/user/dao/UserDaoTest.java
@@ -8,25 +8,37 @@ import java.sql.SQLException;
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.JUnitCore;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.ApplicationContext;
-import org.springframework.context.support.GenericXmlApplicationContext;
 import org.springframework.dao.EmptyResultDataAccessException;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
 import springbook.user.domain.User;
 
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration(locations="/applicationContext.xml")
 public class UserDaoTest {
 	private UserDao dao;
 	private User user1;
 	private User user2;
 	private User user3;
 	
+	ApplicationContext context;
+	
+	@Autowired
+	public void setContext(ApplicationContext context) {
+		this.context = context;
+	}
+
 	public static void main(String[] args){
 		JUnitCore.main("springbook.user.dao.UserDaoTest");
 	}
 
 	@Before
 	public void setUp() {
-		ApplicationContext context = new GenericXmlApplicationContext("applicationContext.xml");
 		
 		this.dao = context.getBean("userDao", UserDao.class);
 		
